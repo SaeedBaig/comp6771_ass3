1919ba8d1399a408101e9552b324c3df6d7601fb
BRL <brl@BRLs-MacBook-Pro.local>
2019-07-18 17:52:28 +1000

    simple interface

--- assignments/dg/graph.h
+++ assignments/dg/graph.h
@@ -1,6 +1,9 @@
 #ifndef ASSIGNMENTS_DG_GRAPH_H_
 #define ASSIGNMENTS_DG_GRAPH_H_
 
+#include <set>
+#include <memory>
+
 namespace gdwg {
 
 template <typename N, typename E>
@@ -8,7 +11,21 @@ class Graph {
  public:
   class const_iterator {};
 
+  bool InsertNode(const N& val);
+  //methods
+  
  private:
+ 	std::set<N> nodes_;
+ 	std::set<Edge> edges_;
+};
+
+class Edge {
+ public:
+
+ private:
+ 	weak_ptr<N> from;
+ 	weak_ptr<N> to;
+ 	E weight;
 };
 
 }  // namespace gdwg

d97adf485f7a9c59fbbd63c8ed08367572036fdc
BRL <brl@BRLs-MacBook-Pro.local>
2019-07-24 01:16:42 +1000

    interface updated

--- assignments/dg/graph.h
+++ assignments/dg/graph.h
@@ -7,13 +7,58 @@
 namespace gdwg {
 
 template <typename N, typename E>
+//n for string
+//e for double
+
 class Graph {
  public:
   class const_iterator {};
 
-  bool InsertNode(const N& val);
+  //constructors
+  gdwg::Graph<N, E>();
+
+  gdwg::Graph<N, E>(
+    std::vector<N>::const_iterator,
+    std::vector<N>::const_iterator
+  );
+
+  gdwg::Graph<N, E>(
+    std::vector<std::tuple<N, N, E>>::const_iterator,
+    std::vector<std::tuple<N, N, E>>::const_iterator
+  );
+
+  gdwg::Graph<N, E>(std::initializer_list<N>);
+
+  gdwg::Graph<N, E>(const gdwg::Graph<N, E>&);
+
+  gdwg::Graph<N, E>(gdwg::Graph<N, E>&&);
+
+  ~gdwg::Graph<N, E>();
+
+  gdwg::Graph<N, E>& operator=(const gdwg::Graph<N, E>&);
+
+  gdwg::Graph<N, E>& operator=(gdwg::Graph<N, E>&&);
+
   //methods
-  
+  bool InsertNode(const N& val);
+  bool InsertEdge(const N& src, const N& dst, const E& w);
+  bool DeleteNode(const N& node);
+  bool Replace(const N& oldData, const N& newData);
+  void MergeReplace(const N& oldData, const N& newData);
+  void Clear();
+  bool IsNode(const N& val);
+  bool IsConnected(const N& src, const N& dst);
+  std::vector<N> GetNodes();
+  std::vector<N> GetConnected(const N& src);
+  std::vector<E> GetWeights(const N& src, const N& dst);
+
+  //iterator methods
+  const_iterator find(const N&, const N&, const E&);
+  bool erase(const N& src, const N& dst, const E& w);
+  const_iterator erase(const_iterator it);
+  const_iterator cbegin();
+  const_iterator cend();
+
  private:
  	std::set<N> nodes_;
  	std::set<Edge> edges_;
